281e9c8210507cf5fb9264c979247b2a2dcbd374
Parth Zile <parthzile01@gmail.com>
2023-10-27 10:41:44 +0530

    Kidz Functional Part II Completed

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ function updateCart() {
 }
 
 for (let i = 0; i < addButtons.length; i++) {
-  addButtons[i].onclick = (e) => {
+  addButtons[i].onclick = () => {
     items[i].quantity++;
     updateCart();
   };
@@ -124,6 +124,42 @@ function updatePrice() {
 cartButton.onclick = () => {
   updatePrice();
 
+
+  for (let index = 0; index < items.length; index++) {
+    if (items[index].quantity != 0) {
+      console.log(
+        "Item name: " +
+          items[index].name +
+          " - Quantity: " +
+          items[index].quantity
+      );
+    }
+  }
+
+  console.log(
+    "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
+  );
+};
+
+var whatsappLink =
+  "https://api.whatsapp.com/send?phone=7420041405&text=Order%20details";
+
+// WhatsApp intergration
+function updateWhatsappLink() {
+  for (let index = 0; index < items.length; index++) {
+    if (items[index].quantity != 0) {
+      whatsappLink += "%0A" + items[index].name + "%20" + items[index].quantity;
+    }
+  }
+  whatsappLink +=
+    "%0A" + "Total%20Price:%20$" + finalDollars + "%20" + finalCents + "c";
+}
+
+cartButton.onclick = () => {
+  updatePrice();
+  // WhatsApp Intergration
+  updateWhatsappLink();
+  window.open(whatsappLink, "_blank");
 
   for (let index = 0; index < items.length; index++) {
     if (items[index].quantity != 0) {
